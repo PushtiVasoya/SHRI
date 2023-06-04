3c3bc66df6fd059b35a12a6f47926ed6c85f0c07
PushtiVasoya <133491531+PushtiVasoya@users.noreply.github.com>
2023-06-04 12:13:57 +0530

    Update Functions.py
    Functions to do all tasks.

--- Functions.py
+++ Functions.py
@@ -1,106 +1,129 @@
-
-import AppOpener as ao    #for opening the apps
-import webbrowser         #to open the urls in browser
-import pyjokes            #to tell jokes
+# all tasks to be performed
+import AppOpener as ao  # for opening the apps
+import webbrowser  # to open the urls in browser
+import pyjokes  # to tell jokes
 
 # For opening apps and websites
-#list of apps
-l=["anydesk","calculator","clock","microsoft excel","notepad","paint","microsoft power point","spotify","microsoft store","microsoft word","terminal"]
-#dictionary of websites with keys as names and values as the urls of websites
-d={"google":"http://www.google.com","gmail":"http://www.gmail.com","youtube":"http://www.youtube.com","facebook":"http://www.facebook.com","instagram":"http://www.instagram.com","linkedin":"http://www.linkedin.com","amazon":"http://www.amazon.com","wikipedia":"http://www.wikipedia.org","twitter":"http://www.twitter.com","netflix":"http://www.netflix.com","stackoverflow":"http://www.stackoverflow.com","whatsapp":"http://www.whatsapp.com","github":"http://www.github.com"}
- 
+l = [
+    "anydesk",
+    "calculator",
+    "clock",
+    "microsoft excel",
+    "notepad",
+    "paint",
+    "microsoft power point",
+    "spotify",
+    "microsoft store",
+    "microsoft word",
+    "terminal",
+]
+d = {
+    "google": "http://www.google.com",
+    "gmail": "http://www.gmail.com",
+    "youtube": "http://www.youtube.com",
+    "facebook": "http://www.facebook.com",
+    "instagram": "http://www.instagram.com",
+    "linkedin": "http://www.linkedin.com",
+    "amazon": "http://www.amazon.com",
+    "wikipedia": "http://www.wikipedia.org",
+    "twitter": "http://www.twitter.com",
+    "netflix": "http://www.netflix.com",
+    "stackoverflow": "http://www.stackoverflow.com",
+    "whatsapp": "http://www.whatsapp.com",
+    "github": "http://www.github.com",
+}
+
+
 def openit(c):
-        for a in c.split(" "):
-            #opening apps
-            if a in l:
-                    ap=ao.open(a)
-                    Speako(f"Opening {a}")
-            #opening websites
-            if a in d:
-                    ap= webbrowser.open(d[a])
-                    Speako(f"Opening {a}")
+    for a in c.split(" "):
+        if a in l:
+            ap = ao.open(a)
+            Speako(f"Opening {a}")
+
+        if a in d:
+            ap = webbrowser.open(d[a])
+            Speako(f"Opening {a}")
+
+
 def closeit(c):
-        for a in c.split(" "):
-            #closing apps
-            if a in l:
-                    ap=ao.close(a)
-                    Speako(f"Closing {a}")
-            #closing websites
-            if a in d:
-                   ap=webbrowser.open("http://www.google.com")
-                   Speak(f"Closing {a}")
+    for a in c.split(" "):
+        if a in l:
+            ap = ao.close(a)
+            Speako(f"Closing {a}")
 
-#youtube search
+        if a in d:
+            ap = webbrowser.open("http://www.chrome.com")
+            Speak(f"Closing {a}")
+
+
+# youtube search
 def youtube(c):
-    #getting the main word to be searched
     if "for" in c:
-        s=c.split("for")[-1]
+        s = c.split("for")[-1]
     elif "play" in c:
-        s=c.split("play")[-1]
+        s = c.split("play")[-1]
     elif "on" in c:
-        s=c.split("play")[1]
-    #defining the url to be searched for
-    url="https://www.youtube.com/results?search_query="+s
-    #opening youtube
+        s = c.split("play")[1]
+    url = "https://www.youtube.com/results?search_query=" + s
     webbrowser.open(url)
-    Speak("Here is what I found for "+s+" on youtube.")
+    Speak("Here is what I found for " + s + " on youtube.")
 
-#google search
+
+# google search
 def google(c):
-    #getting the main word to be searched
-    s=c.split("for")[-1]
-    #defining the url to be searched for
-    url="https://google.com/search?q="+s
+    s = c.split("for")[-1]
+    url = "https://google.com/search?q=" + s
     webbrowser.open(url)
-    Speak("Here is what I found for "+s+" on google.")
+    Speak("Here is what I found for " + s + " on google.")
 
-#google maps
+
+# google maps
 def maps(c):
-    #getting the main word to be searched
     if "is" in c:
-        s=c.split("is")[-1]
+        s = c.split("is")[-1]
     elif "of" in c:
-        s=c.split("of")[-1]
-    #defining the url to be searched for
-    url="https://www.google.com/maps/place/"+s
+        s = c.split("of")[-1]
+    url = "https://www.google.com/maps/place/" + s
     webbrowser.open(url)
-    Speak("Here is what I found for "+s+" on google maps.")
+    Speak("Here is what I found for " + s + " on google maps.")
 
-#spotify search
+
+# spotify search
 def song(c):
-    #getting the main word to be searched
     if "song" in c:
-        s=c.split("song")[-1]
+        s = c.split("song")[-1]
     elif "play" in c:
-        s=c.split("play")[-1]
-    #defining the url to be searched for
-    url="https://open.spotify.com/search/"+s
+        s = c.split("play")[-1]
+    url = "https://open.spotify.com/search/" + s
     webbrowser.open(url)
-    Speak(f"Playing "+s)
+    Speak(f"Playing " + s)
 
 
-#jokes
+# jokes
 def joke():
-    j=pyjokes.get_joke()
+    j = pyjokes.get_joke()
     Speak(j)
+
 
 def commands():
     greet()
     while True:
         try:
-            c=Execute()
-            cd=c.lower()
-            
+            c = Execute()
+            cd = c.lower()
+
             if "how are you" in cd:
                 Speak("I am fine. Thank you!")
                 Speak("How are you?")
-        
+
             elif "your name" in cd:
                 Speak("My name is Shri.")
 
             elif ("who are you" in cd) or ("about you" in cd):
-                Speak("I am Shri - a Smart Human Replicating Intelligence. I am here to make your life easy. Yes,I can't take away your pains but can do your work.")
-               
+                Speak(
+                    "I am Shri - a Smart Human Replicating Intelligence. I am here to make your life easy. Yes,I can't take away your pains but can do your work."
+                )
+
             elif "what is my name" in cd:
                 Speak(f"Your name is {name}.")
 
@@ -112,19 +135,32 @@ def commands():
 
             elif "close" in cd:
                 closeit(cd)
+            elif ("joke" in cd) or ("entertain") in cd:
+                joke()
 
             elif "date" in cd:
-                t=date.today()
-                Speak(f"Today is {t.day}th day of {t.month}th month of the year {t.year}" )
+                t = date.today()
+                Speak(
+                    f"Today is {t.day}th day of {t.month}th month of the year {t.year}"
+                )
 
             elif "time" in cd:
-                t=datetime.now()
-                Speak(f"Time is {t.hour} hour {t.minute} minutes and {t.second} seconds.")
+                t = datetime.now()
+                Speak(
+                    f"Time is {t.hour} hour {t.minute} minutes and {t.second} seconds."
+                )
 
             elif "spotify" in cd:
                 song(cd)
 
-            elif ("search" in cd) or ("google" in cd) or ("more" in cd) or ("show" in cd) or ("tell" in cd) or ("know" in cd):
+            elif (
+                ("search" in cd)
+                or ("google" in cd)
+                or ("more" in cd)
+                or ("show" in cd)
+                or ("tell" in cd)
+                or ("know" in cd)
+            ):
                 google(cd)
 
             elif ("location" in cd) or ("where" in cd):
@@ -133,13 +169,11 @@ def commands():
             elif ("youtube" in cd) or ("play" in cd):
                 youtube(cd)
 
-            elif ("joke" in cd) or ("entertain") in cd:
-                joke()
-
-            elif ("bye" in cd) or ("exit"in cd) or ("quit" in cd) or ("sleep" in cd):
+            elif ("bye" in cd) or ("exit" in cd) or ("quit" in cd) or ("sleep" in cd):
                 Speak("Have a great day!!")
                 break
         except:
             Speak("Sorry, I cannot do that.")
-    
+
+
 commands()
