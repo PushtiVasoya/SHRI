29630d546e2b73cdb1168e1c336a17bf16a33008
PushtiVasoya <133491531+PushtiVasoya@users.noreply.github.com>
2023-06-04 12:21:10 +0530

    Update speak.py
    Converts text to speech and speaks.

--- speak.py
+++ speak.py
@@ -1,40 +1,44 @@
-import pyttsx3   #to convert text to speech
+# convets text to speech and speaks
+import pyttsx3  # to convert text to speech
+from datetime import date, datetime  # to fetch date and time
 
-#Speak with printing
+
+# Speak
+# Speak with printing
 def Speak(t):
-    e=pyttsx3.init("sapi5")
-    v=e.getProperty('voices')
-    e.setProperty('voices',v[0].id)
-    e.setProperty('rate',170)
+    e = pyttsx3.init("sapi5")
+    v = e.getProperty("voices")
+    e.setProperty("voices", v[0].id)
+    e.setProperty("rate", 170)
     print("")
-    print(f"AI:{t}")
+    print(f"SHRI:{t}")
     print("")
     e.say(t)
     e.runAndWait()
-    
-#Speak without printing
+
+
+# Speak without printing
 def Speako(t):
-    e=pyttsx3.init("sapi5")
-    v=e.getProperty('voices')
-    e.setProperty('voices',v[0].id)
-    e.setProperty('rate',170)
+    e = pyttsx3.init("sapi5")
+    v = e.getProperty("voices")
+    e.setProperty("voices", v[0].id)
+    e.setProperty("rate", 170)
     print("")
     print("")
     e.say(t)
     e.runAndWait()
-    
-# basic greetings to user
+
+
 def greet():
     Speak("Jai Shree Krishna")
     Speak("What is your name?")
-    name=Listeno()
-    t=datetime.now()
-
-    # checks time to wish accordingly
-    if 1<=t.hour<12:
-        w="Good Morning"
-    elif 12<=t.hour<=16:
-        w="Good Afternoon"
+    global name
+    name = Listeno()
+    t = datetime.now()
+    if 1 <= t.hour < 12:
+        w = "Good Morning"
+    elif 12 <= t.hour <= 16:
+        w = "Good Afternoon"
     else:
-        w="Good Evening"
+        w = "Good Evening"
     Speak(f"{w},{name}. How may I help you??")
